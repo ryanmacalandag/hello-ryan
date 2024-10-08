30d6ee896c79cce5cdbf16e36b9810e26a8e8db8
Ryan Macalandag <rmacalandag@gmail.com>
2024-10-08 12:03:40 +1100

    list portfolio items from saqme-site data

---
+++ app/data/portfolio.ts
@@ -0,0 +1,38 @@
+type Portfolio = {
+  title: string;
+  url: string;
+}
+export const portfolio:Portfolio[] = [
+  {
+    title: 'Youth Advisory Council',
+    url: '/portfolio/yac.png',
+  },
+  {
+    title: 'Australian Catholic Communications Congress',
+    url: '/portfolio/accc2024-branding.png',
+  },
+  {
+    title: 'Australian Catholic Communications Congress',
+    url: '/portfolio/1.jpg',
+  },
+  {
+    title: 'Australian Catholic Communications Congress',
+    url: '/portfolio/2.jpg',
+  },
+  {
+    title: 'Australian Catholic Communications Congress',
+    url: '/portfolio/3.webp',
+  },
+  {
+    title: 'Australian Catholic Communications Congress',
+    url: '/portfolio/4.jpg',
+  },
+  {
+    title: 'Australian Catholic Communications Congress',
+    url: '/portfolio/5.jpg',
+  },
+  {
+    title: 'Australian Catholic Communications Congress',
+    url: '/portfolio/6.jpg',
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import GetImageCard from "./components/GetImageCard";
-
+import { portfolio } from "./data/portfolio";
+import Image from "next/image";
 
 export default function Home() {
 
@@ -8,23 +8,25 @@ export default function Home() {
       
       <div className="flex flex-col gap-6 amd:gap-12 text-left md:text-center px-0 md:px-24 xl:px-32">
         <h1 className="w-full text-xl font-sans font-bold uppercase tracking-widest">Ryan Macalandag</h1> 
-        <p className="text-2xl lg:text-3xl text-balance text-orange-800 tracking-tight leading-normal italic">I have over 20 years experience in producing quality work for a wide mix of teams and organisations requiring creative thinking, innovative solutions, and strategic execution.</p>
+        <p className="text-2xl lg:text-3xl text-balance text-orange-700 tracking-tight leading-normal italic">I have over 20 years experience in producing quality work for a wide mix of teams and organisations requiring creative thinking, innovative solutions, and strategic execution.</p>
       </div>
 
       <div className="w-full max-w-screen-xl h-fit columns-2 md:columns-3xs lg:columns-3xs gap-3 md:gap-6">
 
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
-        <GetImageCard></GetImageCard>
+        {
+          portfolio.map((item,key) => {
+            return (
+              <Image
+                src={item.url}
+                alt='gallery item'
+                width={320}
+                height={320}
+                className='w-full h-fit rounded-xl aspect-portrait md:aspect-video object-cover mb-3 md:mb-6 shadow-xl bg-black scale-100 hover:scale-105 saturate-75 hover:saturate-100 transition duration-300'
+                key={key}
+              />
+            )
+          })
+        }
 
       </div>
     
