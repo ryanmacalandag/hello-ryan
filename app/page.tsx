1f69c38b38c195685790fa22e6c2e82c9ffd91b6
Ryan Macalandag <rmacalandag@gmail.com>
2025-11-24 15:19:21 +1100

    lights off

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import { portfolio } from "./data/portfolio";
-import Footer from "./components/Footer";
-import PortfolioGallery from "./components/PortfolioGallery";
-import Hero from "./components/Hero";
-
 export default async function Page() {
-
   return (
     <>
-      <Hero></Hero>
+      {/* <Hero></Hero>
       <PortfolioGallery portfolio={portfolio} filter='all' ></PortfolioGallery>
-      <Footer></Footer>
+      <Footer></Footer> */}
     </>
   );
 }
