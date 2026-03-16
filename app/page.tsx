76ce67cb6c6644f763194b0ce09ca1d310733761
Ryan Macalandag <rmacalandag@gmail.com>
2026-03-16 15:29:26 +1100

    back on

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import PortfolioGallery from "./components/PortfolioGallery";
+import { portfolio } from "./data/portfolio";
+
 export default async function Page() {
   return (
     <>
-      {/* <Hero></Hero>
-      <PortfolioGallery portfolio={portfolio} filter='all' ></PortfolioGallery>
-      <Footer></Footer> */}
+      <Hero></Hero>
+      <PortfolioGallery portfolio={portfolio} filter="all"></PortfolioGallery>
+      <Footer></Footer>
     </>
   );
 }
